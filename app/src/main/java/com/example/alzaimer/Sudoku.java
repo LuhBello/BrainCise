370446731d0905f2b473881c9f52f491a25fc433
LuhBello <luisa.alvabello@gmail.com>
2023-05-21 12:18:56 -0500

    5/5 funcionalidades

--- app/src/main/java/com/example/alzaimer/Sudoku.java
+++ app/src/main/java/com/example/alzaimer/Sudoku.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class Sudoku extends AppCompatActivity {
 
@@ -12,8 +19,13 @@ public class Sudoku extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku);
+
     }
 
+    public void IrEmpezar(View view){
+        Intent i = new Intent(this, GameDifficultyActivity.class);
+        startActivity(i);
+    }
     public void IrMenu(View view){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
